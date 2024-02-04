b624eea9396108cf2eb4103ae99df9fee0bdd321
ovoander <trounganderwatt@outlook.com>
2024-02-03 16:03:11 -0800

    refactor css responsive design

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const sidebar = document.getElementById("sidebar");
 const sidebarButton = document.getElementById("sidebar-button");
-const arrowIcon = document.getElementById("arrow-icon");
-
-// const toggleSidebar = () => {
-//   const sidebarWidth = sidebar.offsetWidth;
-//   if (sidebarWidth === 0) {
-//     arrowIcon.style.rotate = "180deg";
-//     sidebar.style.width = "200px";
-//   } else {
-//     arrowIcon.style.rotate = "0deg";
-//     sidebar.style.width = "0px";
-//   }
-// };
 
 const toggleSidebar = () => {
+  const arrowIcon = document.getElementById("arrow-icon");
+  const sidebar = document.getElementById("sidebar");
   const isOpen = sidebar.classList.contains("open");
   if (!isOpen) {
     arrowIcon.style.rotate = "180deg";
