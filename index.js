55a4605d37cadd11d0bbfe274681ad158b90f876
ovoander <trounganderwatt@outlook.com>
2024-02-03 16:01:00 -0800

    made sidebar responsive

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const sidebar = document.getElementById("sidebar");
 const sidebarButton = document.getElementById("sidebar-button");
 const arrowIcon = document.getElementById("arrow-icon");
 
+// const toggleSidebar = () => {
+//   const sidebarWidth = sidebar.offsetWidth;
+//   if (sidebarWidth === 0) {
+//     arrowIcon.style.rotate = "180deg";
+//     sidebar.style.width = "200px";
+//   } else {
+//     arrowIcon.style.rotate = "0deg";
+//     sidebar.style.width = "0px";
+//   }
+// };
+
 const toggleSidebar = () => {
-  const sidebarWidth = sidebar.offsetWidth;
-  if (sidebarWidth === 0) {
+  const isOpen = sidebar.classList.contains("open");
+  if (!isOpen) {
     arrowIcon.style.rotate = "180deg";
-    sidebar.style.width = "200px";
+    sidebar.classList.add("open");
   } else {
     arrowIcon.style.rotate = "0deg";
-    sidebar.style.width = "0px";
+    sidebar.classList.remove("open");
   }
 };
 
