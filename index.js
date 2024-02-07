d3a4647a5ddaa77db55bf5d0f2a6503965d371c8
ovoander <trounganderwatt@outlook.com>
2024-02-06 17:47:50 -0800

    refactor

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const sidebarButton = document.getElementById("sidebar-button");
 const toggleSidebar = () => {
   const isOpen = sidebar.classList.contains("open");
   if (!isOpen) {
-    arrowIcon.style.rotate = "180deg";
+    arrowIcon.classList.add("open");
     sidebar.classList.add("open");
   } else {
-    arrowIcon.style.rotate = "0deg";
+    arrowIcon.classList.remove("open");
     sidebar.classList.remove("open");
   }
 };
@@ -17,8 +17,8 @@ const handleDocumentClick = (event) => {
   if (!event.target.closest("#header") && !event.target.closest("#sidebar")) {
     const isOpen = sidebar.classList.contains("open");
     if (isOpen) {
+      arrowIcon.classList.remove("open");
       sidebar.classList.remove("open");
-      arrowIcon.style.rotate = "0deg";
     }
   }
 };
