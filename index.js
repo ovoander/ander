67b169ab0d57ac455522936643d78315680e7719
ovoander <trounganderwatt@outlook.com>
2024-02-06 18:52:11 -0800

    formatted index.js and styles.css

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const arrowIcon = document.getElementById("arrow-icon");
-const sidebar = document.getElementById("sidebar");
-const sidebarButton = document.getElementById("sidebar-button");
+const arrowIcon = document.getElementById('arrow-icon')
+const sidebar = document.getElementById('sidebar')
+const sidebarButton = document.getElementById('sidebar-button')
 
-const toggleSidebar = () => {
-  arrowIcon.classList.toggle("open");
-  sidebar.classList.toggle("open");
-};
+function toggleSidebar() {
+  arrowIcon.classList.toggle('open')
+  sidebar.classList.toggle('open')
+}
 
-const handleDocumentClick = (event) => {
-  const isOpen = sidebar.classList.contains("open");
+function handleDocumentClick(event) {
+  const isOpen = sidebar.classList.contains('open')
   const isOutsideClick =
-    !event.target.closest("#header") && !event.target.closest("#sidebar");
+    !event.target.closest('#header') && !event.target.closest('#sidebar')
 
   if (isOpen && isOutsideClick) {
-    toggleSidebar();
+    toggleSidebar()
   }
-};
+}
 
-document.addEventListener("click", handleDocumentClick);
-sidebarButton.addEventListener("click", toggleSidebar);
+document.addEventListener('click', handleDocumentClick)
+sidebarButton.addEventListener('click', toggleSidebar)
