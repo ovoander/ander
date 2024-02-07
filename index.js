44e208b236670af0c8411ea4f31ca33a49deb86d
ovoander <trounganderwatt@outlook.com>
2024-02-07 08:54:59 -0800

    formatted index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const arrowIcon = document.getElementById('arrow-icon')
-const sidebar = document.getElementById('sidebar')
-const sidebarButton = document.getElementById('sidebar-button')
+const arrowIcon = document.getElementById("arrow-icon");
+const sidebar = document.getElementById("sidebar");
+const sidebarButton = document.getElementById("sidebar-button");
 
 function toggleSidebar() {
-  arrowIcon.classList.toggle('open')
-  sidebar.classList.toggle('open')
+  arrowIcon.classList.toggle("open");
+  sidebar.classList.toggle("open");
 }
 
 function handleDocumentClick(event) {
-  const isOpen = sidebar.classList.contains('open')
+  const isOpen = sidebar.classList.contains("open");
   const isOutsideClick =
-    !event.target.closest('#header') && !event.target.closest('#sidebar')
+    !event.target.closest("#header") && !event.target.closest("#sidebar");
 
   if (isOpen && isOutsideClick) {
-    toggleSidebar()
+    toggleSidebar();
   }
 }
 
-document.addEventListener('click', handleDocumentClick)
-sidebarButton.addEventListener('click', toggleSidebar)
+document.addEventListener("click", handleDocumentClick);
+sidebarButton.addEventListener("click", toggleSidebar);
