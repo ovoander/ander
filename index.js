bb1d83f3a481685565e8d4d67c25556cbd869072
ovoander <trounganderwatt@outlook.com>
2024-02-07 08:56:22 -0800

    converted named functions to arrow syntax

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const arrowIcon = document.getElementById("arrow-icon");
 const sidebar = document.getElementById("sidebar");
 const sidebarButton = document.getElementById("sidebar-button");
 
-function toggleSidebar() {
+const toggleSidebar = () => {
   arrowIcon.classList.toggle("open");
   sidebar.classList.toggle("open");
-}
+};
 
-function handleDocumentClick(event) {
+const handleDocumentClick = (event) => {
   const isOpen = sidebar.classList.contains("open");
   const isOutsideClick =
     !event.target.closest("#header") && !event.target.closest("#sidebar");
@@ -15,7 +15,7 @@ function handleDocumentClick(event) {
   if (isOpen && isOutsideClick) {
     toggleSidebar();
   }
-}
+};
 
 document.addEventListener("click", handleDocumentClick);
 sidebarButton.addEventListener("click", toggleSidebar);
